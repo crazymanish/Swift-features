6e3d7470071db1f42e0faf11329fb97e0b3877f1
Manish Rathi <manishrathi19902013@gmail.com>
2020-01-04 23:33:34 +0100

    Added isMultiple example

---
+++ Source/Swift-features.playground/Pages/isMultiple.xcplaygroundpage/Contents.swift
@@ -0,0 +1,7 @@
+//: [Previous](@previous)
+import Foundation
+
+//Details: https://github.com/apple/swift-evolution/blob/master/proposals/0225-binaryinteger-iseven-isodd-ismultiple.md
+//SwiftLint: https://github.com/realm/SwiftLint/blob/master/Rules.md#L11584
+
+//: [Next](@next)
