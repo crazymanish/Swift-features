bc839d72706c4771773afbb539bdbb9edb73bfcd
Manish Rathi <manishrathi19902013@gmail.com>
2020-01-06 19:09:01 +0100

    Updated opaque-type

---
+++ Source/Swift-features.playground/Pages/OpaqueResultTypes.xcplaygroundpage/Contents.swift
@@ -0,0 +1,9 @@
+//: [Previous](@previous)
+import Foundation
+
+//Reference: https://docs.swift.org/swift-book/LanguageGuide/OpaqueTypes.html
+//Details: https://github.com/apple/swift-evolution/blob/master/proposals/0244-opaque-result-types.md
+
+//Rules: https://github.com/apple/swift-evolution/blob/master/proposals/0244-opaque-result-types.md#implementing-a-function-returning-an-opaque-type
+
+//: [Next](@next)
