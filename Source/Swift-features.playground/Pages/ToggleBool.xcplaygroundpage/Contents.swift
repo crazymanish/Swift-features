5ce4c7981253a634bbfb2659aee4681ee328c4e0
Manish Rathi <manishrathi19902013@gmail.com>
2020-01-06 18:19:11 +0100

    Toggle Example.

---
+++ Source/Swift-features.playground/Pages/ToggleBool.xcplaygroundpage/Contents.swift
@@ -0,0 +1,8 @@
+//: [Previous](@previous)
+import Foundation
+
+//Reference: https://developer.apple.com/documentation/swift/bool/2994863-toggle
+//Details: https://github.com/apple/swift-evolution/blob/master/proposals/0199-bool-toggle.md
+//SwiftLint: https://github.com/realm/SwiftLint/blob/master/Rules.md#L20236
+
+//: [Next](@next)
