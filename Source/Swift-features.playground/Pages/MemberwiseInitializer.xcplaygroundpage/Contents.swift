6a22507c198a15284de4799da0becb43860e5e42
Manish Rathi <manishrathi19902013@gmail.com>
2020-01-06 18:37:15 +0100

    memberwise initializer example

---
+++ Source/Swift-features.playground/Pages/MemberwiseInitializer.xcplaygroundpage/Contents.swift
@@ -0,0 +1,9 @@
+//: [Previous](@previous)
+import Foundation
+
+//Synthesize default values for the memberwise initializer
+//Details: https://github.com/apple/swift-evolution/blob/master/proposals/0242-default-values-memberwise.md
+
+//Limitation: Access Control -> 'internal' always
+
+//: [Next](@next)
