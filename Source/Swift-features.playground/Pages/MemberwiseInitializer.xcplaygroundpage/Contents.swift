9daf49c1921ffb6d9f03948f5761d3051976538a
Manish Rathi <manishrathi19902013@gmail.com>
2020-01-06 18:39:23 +0100

    CleanUp

--- Source/Swift-features.playground/Pages/MemberwiseInitializer.xcplaygroundpage/Contents.swift
+++ Source/Swift-features.playground/Pages/MemberwiseInitializer.xcplaygroundpage/Contents.swift
@@ -5,5 +5,6 @@ import Foundation
 //Details: https://github.com/apple/swift-evolution/blob/master/proposals/0242-default-values-memberwise.md
 
 //Limitation: Access Control -> 'internal' always
+//let sparky = Dog(age: 2, name: "sparky")
 
 //: [Next](@next)
---
+++ Source/Swift-features.playground/Pages/MemberwiseInitializer.xcplaygroundpage/Sources/Dog.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+public struct Dog {
+    public var age: Int
+    public var name: String
+}
